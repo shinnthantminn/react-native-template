7e21f521b008217cc382cb87ec29db2f5b4d7839
shinnthantminn <shinthantmin32@gmail.com>
2023-10-03 01:01:10 +0630

    template

---
+++ template/App.tsx
@@ -0,0 +1,16 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
